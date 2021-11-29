c31d5f88394c4437ad16ce9c919d120b7e75f99b
VitorAguiar <vitor@ib.usp.br>
2021-11-29 09:47:04 -0500

    update plot

--- plot.R
+++ plot.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggsci)
+library(ggthemes)
 library(ggrepel)
 library(RColorBrewer)
 library(cowplot)
@@ -97,31 +98,6 @@ logfc %>%
 
 ggsave("./plots/foldchange_summary.png", width = 5)
 
-
-
-#PCA on expression data
-
-pca <- "./data/pca/pheno_pcs.pca" %>%
-    read_delim(delim = " ") %>%
-    pivot_longer(-1, names_to = "condition_id") %>%
-    mutate(pc = str_extract(SampleID, "(PC\\d$)")) %>%
-    select(pc, condition_id, value) %>%
-    pivot_wider(names_from = pc, values_from = value) %>%
-    mutate(condition_id = factor(condition_id, 
-                                 levels = c("16hr_resting", "24hr_IgG",
-                                            "72hr_IgG", "24hr_RSQ", "72hr_RSQ")))
-
-ggplot(pca, aes(PC1, PC2, color = condition_id)) +
-    geom_point(size = 5) +
-    scale_color_manual(values = c("16hr_resting" = "grey35",
-                                  "24hr_IgG" = "slateblue",
-                                  "72hr_IgG" = "cornflowerblue",
-                                  "24hr_RSQ" = "tomato3",
-                                  "72hr_RSQ" = "salmon")) +
-    theme_bw()
-
-ggsave("./plots/pca_bcell_expression.png")
-
 # Correlations between conditions
 gene_names <- read_tsv("./data/transc_to_gene.tsv") %>%
     distinct(gene_id, gene_name)
@@ -163,51 +139,60 @@ ggplot(gene_v2_df, aes(`16hr_resting`, cpm)) +
 
 ggsave("./plots/scatter_resting_conditions.png")
 
+# Comparison between log2 FC
 
 condition_df <- logfc %>%
+    separate(condition_id, c("time", "stim"), sep = "_") %>%
     select(-cpm) %>%
-    pivot_wider(names_from = condition_id, values_from = log2fc)
+    pivot_wider(names_from = time, values_from = log2fc)
 
-igG_diff <- condition_df %>%
-    filter(`24hr_IgG` < -50 & `72hr_IgG` > -20 | 
-           `72hr_IgG` < -50 & `24hr_IgG` > -20) %>%
+stim_diffs <- condition_df %>%
+    filter(`24hr` < -60 | `72hr` < -60) %>%
     left_join(gene_names)
 
-rsq_diff <- condition_df %>%
-    filter(`24hr_RSQ` < -50 & `72hr_RSQ` > -20 | 
-               `72hr_RSQ` < -50 & `24hr_RSQ` > -20) %>%
-    left_join(gene_names)
 
-igg <- ggplot(filter(condition_df, ! gene_id %in% igG_diff$gene_id),
-       aes(`24hr_IgG`, `72hr_IgG`)) +
-    geom_abline(color = "grey", linetype = 2) +
-    geom_point(size = .5) +
-    geom_point(data = igG_diff, 
-               aes(`24hr_IgG`, `72hr_IgG`, color = gene_type),
-               size = .5) +
-    scale_color_npg() +
+ggplot(anti_join(condition_df, stim_diffs),
+       aes(`24hr`, `72hr`)) +
+    geom_abline(linetype = 2, color = "grey") +
+    geom_hline(yintercept = 0, linetype = 2, color = "grey") +
+    geom_vline(xintercept = 0, linetype = 2, color = "grey") +
+    geom_point(alpha = .25) +
+    geom_point(data = stim_diffs,
+                    aes(`24hr`, `72hr`, color = gene_type),
+               alpha = .5) +
+    scale_color_gdocs() +
+    facet_wrap(~stim) +
     theme_bw() +
     theme(panel.grid = element_blank()) +
-    labs(x = expression(paste("Log"[2], "FC (24hr)")),
-         y = expression(paste("Log"[2], "FC (72hr)")),
-         title = "IgG")
+    labs(color = "Gene type") +
+    guides(color = guide_legend(override.aes = list(alpha = 1)))
 
-rsq <- ggplot(filter(condition_df, ! gene_id %in% rsq_diff$gene_id),
-       aes(`24hr_RSQ`, `72hr_RSQ`)) +
-    geom_abline(color = "grey", linetype = 2) +
-    geom_point(size = .5) +
-    geom_point(data = rsq_diff, 
-               aes(`24hr_RSQ`, `72hr_RSQ`, color = gene_type),
-               size = .5) +
-    scale_color_npg() +
-    theme_bw() +
-    theme(panel.grid = element_blank()) +
-    labs(x = expression(paste("Log"[2], "FC (24hr)")),
-         y = expression(paste("Log"[2], "FC (72hr)")),
-         title = "RSQ")
+ggsave("./plots/fc_24vs72.png", width = 6, height = 2.5)
 
-plot_grid(igg, rsq, ncol = 1)
-ggsave("./plots/fc_24vs72.png", width = 5, height = 4)
+
+#PCA on expression data
+
+pca <- "./data/pca/pheno_pcs.pca" %>%
+    read_delim(delim = " ") %>%
+    pivot_longer(-1, names_to = "condition_id") %>%
+    mutate(pc = str_extract(SampleID, "(PC\\d$)")) %>%
+    select(pc, condition_id, value) %>%
+    pivot_wider(names_from = pc, values_from = value) %>%
+    mutate(condition_id = factor(condition_id, 
+                                 levels = c("16hr_resting", "24hr_IgG",
+                                            "72hr_IgG", "24hr_RSQ", "72hr_RSQ")))
+
+ggplot(pca, aes(PC1, PC2, color = condition_id)) +
+    geom_point(size = 5) +
+    scale_color_manual(values = c("16hr_resting" = "grey35",
+                                  "24hr_IgG" = "slateblue",
+                                  "72hr_IgG" = "cornflowerblue",
+                                  "24hr_RSQ" = "tomato3",
+                                  "72hr_RSQ" = "salmon")) +
+    theme_bw()
+
+ggsave("./plots/pca_bcell_expression.png")
+
 
 # MGB biobank
 
@@ -248,7 +233,7 @@ all_cols <- c(mgb_cols, afr_cols, eur_cols, sas_cols, eas_cols, amr_cols)
 pca_genos <-
     file.path("/lab-share/IM-Gutierrez-e2/Public/vitor/ase/mgb_biobank/results",
     "allchr.merged.pruned.pca.eigenvec") %>%
-    read_table(col_names = FALSE) %>%
+    read_table2(col_names = FALSE) %>%
     select(-1) %>%
     select(X2:X6) %>%
     setNames(c("sample_id", paste0("PC", 1:4)))
@@ -352,12 +337,47 @@ ggsave("./plots/kmeans_accuracy.png", height = 3, width = 5)
 clusters_continent %>%
     filter(k == 7, continent == "EUR")
 
+### Illustration (slides)
+
+k5 <- kmeans(pca_matrix, 5)
+
+k5_df <- pca_df %>%
+    add_column(cluster = k5$cluster)
+
+k5_cts <- as.data.frame(k5$centers)
+
+pa <- ggplot(k5_df, aes(PC1, PC2)) +
+    geom_point(alpha = .05) +
+    theme_bw()
+
+pb <- ggplot(k5_df, aes(PC1, PC2)) +
+    geom_point(alpha = .05) +
+    geom_point(data = k5_cts, 
+               aes(PC1, PC2, color = factor(1:5)), 
+               size = 4) +
+    scale_color_nejm(guide = NULL) +
+    theme_bw() +
+    theme(panel.grid = element_blank())
+
+pc <- ggplot(k5_df, aes(PC1, PC2, color = factor(cluster))) +
+    geom_point(alpha = .2) +
+    scale_color_nejm(guide = NULL) +
+    theme_bw() +
+    theme(panel.grid = element_blank())
+
+plot_grid(pa, pb, pc, ncol = 1)
+ggsave("./plots/kmeans_illustration.png", width = 4, height = 6)
+
+###
+
+
+
 # Select MGB individuals with high EUR ancestry
 mgb_eur <- clusters_df %>%
     filter(dataset == "MGB", k == 7, cluster %in% c(3L, 7L)) %>%
     pull(sample_id)
 
-continent_colors <- all_cols[c("YRI", "GBR", "BEB", "CHS", "PEL")] %>%
+continent_colors <- all_cols[c("YRI", "TSI", "PJL", "CDX", "PEL")] %>%
     setNames(levels(clusters_continent$continent))
 
 clusters_plot_df <- clusters_df %>%
@@ -410,11 +430,31 @@ scores_eur_df <- scores_df %>%
 
 
 ggplot(scores_df, aes(het_score, het_score_wt)) +
-    geom_jitter(size = .75, alpha = .1) +
+    geom_point(size = .75, alpha = .1) +
     theme_bw()  +
     labs(x = "Heterozigosity score", y = "Weighted heterozygosity score")
 
-ggsave("./plots/het_scores_jitterplot.png", width = 5, height = 4)
+ggsave("./plots/het_scores_points.png", width = 5, height = 4)
+
+
+score_colors <- 
+    bind_rows(unweighted = top_n(scores_df, 12, het_score), 
+              weighted = top_n(scores_df, 12, het_score_wt), .id = "score") %>%
+    select(-top_eur) %>%
+    group_by(sample_id) %>%
+    mutate(score = ifelse(n_distinct(score) > 1, "both", score))
+
+
+ggplot(scores_df, aes(het_score, het_score_wt)) +
+    geom_point(size = .75, alpha = .1) +
+    geom_point(data = score_colors, 
+               aes(het_score, het_score_wt, color = score),
+               size = 2.5) +
+    scale_color_uchicago() +
+    theme_bw() +
+    labs(x = "Heterozigosity score", y = "Weighted heterozygosity score") 
+
+ggsave("./plots/het_scores_points_top12.png", width = 6, height = 4)
 
 
 scores_df %>%
