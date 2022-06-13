783de6908e0150348033a47c2e0b4b6c93353895
VitorAguiar <vitor@ib.usp.br>
2022-06-13 15:38:25 -0400

    add splicing analysis

---
+++ splicing/prepare_leafviz_annot.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+$HOME/leafcutter/leafviz/gtf2leafcutter.pl \
+    -o gencode_hg38 \
+    /lab-share/IM-Gutierrez-e2/Public/References/Annotations/hsapiens/gencode.v38.primary_assembly.annotation.gtf
