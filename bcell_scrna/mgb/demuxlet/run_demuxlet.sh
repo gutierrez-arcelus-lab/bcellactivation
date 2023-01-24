84325a149f7e3969dedbce5c0a2eaf28fb99d88f
VitorAguiar <vitor@ib.usp.br>
2023-01-24 17:39:14 -0500

    add scripts

---
+++ bcell_scrna/mgb/demuxlet/run_demuxlet.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+source /programs/biogrids.shrc
+
+CITESEQ=/lab-share/IM-Gutierrez-e2/Public/Lab_datasets/B_cells_citeseq/SN0268787/broad/hptmp/curtism/bwh10x/KW10456_Maria/221101_10X_KW10456_bcl/cellranger-6.1.1/GRCh38/BRI-1984/outs
+BAM=${CITESEQ}/possorted_genome_bam.bam
+BARCODES=${CITESEQ}/filtered_feature_bc_matrix/barcodes.tsv.gz
+VCF=./data/allchr.mgb.vcf.gz
+OUT=./demuxlet_results
+LOG=${OUT}.log
+
+demuxlet --sam $BAM \
+    --vcf $VCF --field GP \
+    --group-list $BARCODES \
+    --min-BQ 20 \
+    --min-uniq 10 \
+    --alpha 0 --alpha 0.5 \
+    --out $OUT 2> $LOG
