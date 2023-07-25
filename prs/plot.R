99d6049cc05584fc2cd719baa767fa435dc065ba
VitorAguiar <vitor@ib.usp.br>
2023-07-25 18:25:08 -0400

    add roc and auc plot

--- prs/plot.R
+++ prs/plot.R
@@ -134,7 +134,8 @@ pca_legend_2 <-
 	       aes(color = population), shape = 1) +
     scale_color_manual(values = all_cols[! names(all_cols) %in% c("Control", "SLE")]) +
     theme_bw() +
-    theme(legend.margin = margin(t = -6.5, unit = "cm")) +
+    theme(legend.margin = margin(t = -6.5, unit = "cm"),
+	  legend.spacing.y = unit(0, 'cm')) +
     labs(color = NULL) + 
     guides(color = guide_legend(override.aes = list(size = 2, stroke = 1.5)))
 
@@ -179,59 +180,152 @@ pca_p <-
 ggsave("./plots/pca.png", pca_p, width = 10, dpi = 600)
 
 # UMAP
-mgb_umap <- select(pca_df, PC1:PC7) |>
-    umap(n_threads = future::availableCores(),
-	 seed = 1)
+continental_colors <- 
+    c("AFR" = all_cols[["MSL"]],
+      "AMR" = all_cols[["PEL"]],
+      "EAS" = all_cols[["CHS"]],
+      "EUR" = all_cols[["GBR"]],
+      "SAS" = all_cols[["BEB"]])
+
+race_colors <- 
+    c("White" = "cornflowerblue", 
+      "Spanish" = "midnightblue", 
+      "Other" = "grey",
+      "American Indian or Alaska Native" = "black", 
+      "Asian" = "purple", 
+      "Black" = "brown",
+      "Native Hawaiian or Other Pacific Islander" = "goldenrod",
+      "Hispanic or Latino" = "hotpink")
+
+shapes <- 
+    tibble(group = levels(umap_df$population)) |>
+    mutate(shape = case_when(group %in% c("Control", "SLE") ~ 3L,
+			     TRUE ~ 1L)) |>
+    deframe()
+
+set.seed(1)
+mgb_umap <- select(pca_df, PC1:PC8) |>
+    umap(n_threads = future::availableCores())
 
 umap_df <- 
     bind_cols(select(pca_df, subject_id, population), 
 	      as.data.frame(mgb_umap)) |>
     as_tibble()
+    
+umap_df <- umap_df |>
+    mutate(population = fct_relevel(population, "SLE", after = Inf)) |>
+    arrange(population) |>
+    mutate(subject_id = fct_inorder(subject_id))
+
+legend_order <- 
+    umap_df$population |>
+    unique() |>
+    fct_inorder() |>
+    fct_relevel("SLE", after = 1L) |>
+    sort() |>
+    as.character()
 
 plot_umap <- 
-    ggplot(data = umap_df, 
-	   aes(V1, V2, color = population)) +
-    geom_point(data = filter(umap_df, population == "Control"),
-	       size = .75, shape = 3) +
-    geom_point(data = filter(umap_df, ! population %in% c("Control", "SLE")),
-	       size = .75, shape = 1) +
-    geom_point(data = filter(umap_df, population == "SLE"),
-	       size = .75, shape = 3) +
-    scale_color_manual(values = all_cols) +
-    theme_bw() +
-    theme(panel.grid = element_blank(),
-	  legend.position = "none") +
-    labs(x = "UMAP 1", y = "UMAP 2")
-
-race_colors <- 
-    c("White" = eur_cols[[2]], 
-      "Spanish" = "cornflowerblue", 
-      "Other" = "black",
-      "American Indian or Alaska Native" = "pink", 
-      "Asian" = "purple", 
-      "Black" = "orange",
-      "Native Hawaiian or Other Pacific Islander" = "forestgreen",
-      "Hispanic or Latino" = "magenta")
+    ggplot(data = umap_df,
+	   aes(V1, V2, color = population, shape = population)) +
+	geom_point(size = 1) +
+	scale_color_manual(values = all_cols, 
+			   breaks = legend_order) +
+	scale_shape_manual(values = shapes,
+			   breaks = legend_order) +
+	theme_minimal() +
+	theme(panel.grid = element_blank(),
+	      axis.text = element_blank(),
+	      plot.title = element_text(size = 12),
+	      plot.margin = margin(.5, .5, 1, .5, unit = 'cm'),
+	      plot.background = element_rect(color = "white", fill = "white")) +
+	labs(x = "UMAP 1", 
+	     y = "UMAP 2", 
+	     color = "Group:", 
+	     shape = "Group:",
+	     title = "UMAP with 8 PCs using\n1000 Genomes as reference panel") +
+	guides(color = guide_legend(override.aes = list(size = 2, stroke = 1.25)))
 
 
-plot_umap_race <- umap_df |>
+umap_race_df <- umap_df |>
     inner_join(sle_data) |>
     sample_frac(1L) |>
     mutate(subject_id = fct_inorder(subject_id)) |>
-    select(subject_id, race, V1, V2) |>
-    ggplot(aes(V1, V2, color = race)) +
-    geom_point(size = .75, shape = 1) +
+    select(subject_id, race, V1, V2) 
+
+plot_umap_race <-
+    ggplot(umap_race_df, 
+	   aes(V1, V2, color = race)) +
+    geom_point(size = 1, shape = 1) +
     scale_color_manual(values = race_colors,
 		       labels = function(x) str_wrap(x, width = 16),
 		       na.value = "grey") +
-    theme_bw() +
-    theme(panel.grid = element_blank()) +
-    labs(x = "UMAP 1", y = "UMAP 2") +
-    guides(color = guide_legend(override.aes = list(size = 2, stroke = 1.5)))
+    theme_minimal() +
+    theme(panel.grid = element_blank(),
+	  axis.text = element_blank(),
+	  plot.title = element_text(size = 12),
+	  plot.margin = margin(.5, .5, 1, .5, unit = 'cm'),
+	  plot.background = element_rect(color = "white", fill = "white")) +
+    labs(x = "UMAP 1", 
+	 y = "UMAP 2", 
+	 title = "UMAP colored by reported race\nin MGB Biobank") +
+    guides(color = guide_legend(override.aes = list(size = 2, stroke = 1.25)))
 
-ggsave("./plots/umap.png", plot_umap, width = 5, height = 5)
-ggsave("./plots/umap_race.png", plot_umap_race, width = 7, height = 5)
+# Ancestry proportions from ADMIXTURE
+admix <- 
+    "./admixture/admix_results.tsv" |>
+    read_tsv(col_types = c(subject_id = "c")) |>
+    left_join(umap_prs_df) |>
+    select(subject_id, group, race, EUR:SAS, prs) |>
+    pivot_longer(EUR:SAS, names_to = "ancestry")
+    
 
+## Check UMAP colored by ancestry proportions
+umap_anc_df <- 
+    filter(umap_df, population %in% c("SLE", "Control")) |>
+    left_join(select(admix, subject_id, ancestry, value), join_by(subject_id))
+
+umaps_anc <- 
+    map(names(continental_colors), 
+	function(anc) {
+	    ggplot(data = filter(umap_anc_df, ancestry == anc),
+		   aes(x = V1, y = V2)) +
+	geom_point(aes(color = value), size = .25) +
+	scale_color_gradient(low = "grey96", high = continental_colors[anc],
+			     guide = guide_colorbar(barheight = .25),
+			     breaks = c(0, .5, 1),
+			     limits = c(0, 1)) +
+	facet_wrap(~ancestry) +
+	theme_bw() +
+	theme(axis.text = element_blank(),
+	      axis.ticks = element_blank(),
+	      panel.grid = element_blank(),
+	      legend.position = "bottom",
+	      legend.margin = margin(t = -.25, b = 0, unit = "cm"),
+	      plot.margin = margin(t = .5, unit = "cm"),
+	      plot.background = element_rect(color = "white", fill = "white")) +
+	labs(x = NULL, y = NULL, color = NULL)}) |>
+    {function(x) plot_grid(plotlist = x, nrow = 1)}() +
+    labs(title = "Proportion of each ancestry as estimated with ADMIXTURE") +
+    theme(plot.title = element_text(size = 12, hjust = 0),
+	  plot.background = element_rect(color = "white", fill = "white"))
+       
+umap_out <- 
+    plot_grid(
+	      plot_grid(plot_umap, plot_umap_race, nrow = 1, labels = c("A)", "B)")),
+	      plot_grid(NULL, umaps_anc, NULL, 
+			nrow = 1, rel_widths = c(.1, 1, .1),
+			labels = c("C)")),
+	      ncol = 1, rel_heights = c(1, .5)) +
+    theme(plot.background = element_rect(color = "white", fill = "white"))
+
+ggsave("plots/umaps.png",
+       umap_out,
+       width = 11, height = 7, dpi = 600)
+
+
+
+# PRS
 prs_df <- 
     "./common_data/prs_p+t_5e-4.tsv" |>
     read_tsv(col_type = c("subject_id" = "c"))
@@ -240,35 +334,8 @@ umap_prs_df <- umap_df |>
     filter(population %in% c("SLE", "Control")) |>
     left_join(prs_df, join_by(subject_id)) 
 
-umap_prs <- |>
-    ggplot(umap_prs_df, aes(V1, V2, color = prs)) +
-    geom_point(size = .25) +
-    scale_color_continuous(low = "beige", high = "midnightblue",
-			   guide = guide_colorbar(barheight = .25)) +
-    theme_bw() +
-    theme(panel.grid = element_blank(),
-	  legend.position = "top") +
-    labs(x = "UMAP 1", y = "UMAP 2",
-	 color = "PRS:")
-    
-ggsave("./plots/umap_prs.png", umap_prs, width = 5, height = 5)
-
-admix <- 
-    "./admixture/admix_results.tsv" |>
-    read_tsv(col_types = c(subject_id = "c")) |>
-    left_join(umap_prs_df) |>
-    select(subject_id, group, race, EUR:SAS, prs) |>
-    pivot_longer(EUR:SAS, names_to = "ancestry")
-
 admix_filt <- admix |>
     filter(value > 0.1)
-    
-continental_colors <- 
-    c("AFR" = all_cols[["MSL"]],
-      "AMR" = all_cols[["PEL"]],
-      "EAS" = all_cols[["CHS"]],
-      "EUR" = all_cols[["GBR"]],
-      "SAS" = all_cols[["BEB"]])
 
 p1 <- 
     ggplot(data = admix_filt,
@@ -327,3 +394,71 @@ prs_race_df |>
     ungroup() |>
     arrange(race, desc(m)) |>
     print(n = Inf)
+
+
+# AUC
+library(pROC)
+
+roc_full <- roc(prs_df$group, prs_df$prs)
+
+roc_df <- 
+    roc_full[c("sensitivities", "specificities")] |>
+    as.data.frame() |> 
+    as_tibble() |>
+    mutate(ancestry = "Total") |>
+    select(ancestry, everything())
+
+rocs_ancestries <- admix |>
+    filter(value >= .75) |> 
+    filter(ancestry %in% c("AFR", "EAS", "EUR")) |>
+    {function(x) split(x, x$ancestry)}() |>
+    map(function(x) roc(x$group, x$prs)) |>
+    map(smooth) |>
+    map(function(x) x[c("sensitivities", "specificities")]) |>
+    map(as.data.frame) |>
+    map_df(as_tibble, .id = "ancestry")
+
+roc_colors <- c("Total" = "black", continental_colors[c("AFR", "EAS", "EUR")])
+
+roc_p <- 
+    bind_rows(roc_df, rocs_ancestries) |>
+    mutate(ancestry = fct_inorder(ancestry)) |>
+    ggplot(aes(x = specificities, y = sensitivities, color = ancestry)) +
+    geom_line(linetype = 2, linewidth = 1.25) +
+    scale_x_reverse() +
+    scale_color_manual(values = roc_colors) +
+    theme_minimal() +
+    theme(panel.grid = element_line(color = "grey96"),
+	  panel.grid.minor.x = element_blank(),
+	  panel.grid.minor.y = element_blank(),
+	  plot.background = element_rect(color = "white", fill = "white")) +
+    labs(x = "Specificity", y = "Sensitivity", color = "Group:")
+
+auc_df <- admix |>
+    filter(value >= .75) |> 
+    filter(ancestry %in% c("AFR", "EAS", "EUR")) |>
+    {function(x) split(x, x$ancestry)}() |>
+    map(function(x) roc(x$group, x$prs)) |>
+    map(auc) |>
+    map(as.numeric) |>
+    c("Total" = auc(roc_full)) |>
+    bind_rows(.id = "ancestry") |>
+    pivot_longer(everything(), names_to = "ancestry", values_to = "auc")
+
+auc_p <- 
+    ggplot(data = auc_df, aes(ancestry, auc)) +
+    geom_col(aes(fill = ancestry)) +
+    scale_fill_manual(values = roc_colors) +
+    scale_y_continuous(limits = c(0, 1),
+		       breaks = seq(0, 1, .2)) +
+    theme_minimal() +
+    theme(legend.position = "none",
+	  plot.background = element_rect(color = "white", fill = "white")) +
+    labs(y = "AUC")
+
+ggsave("./plots/roc.png", 
+       (roc_p | auc_p) + plot_layout(nrow = 1, widths = c(1, .5)), 
+       width = 8, height = 4)
+
+
+
