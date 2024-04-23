61a01fc4fe5a807175e76840141cde7029b74d59
VitorAguiar <vitor@ib.usp.br>
2024-04-23 16:04:08 -0400

    update mbv plot

--- bcell_rnaseq/2-mbv/plot.R
+++ bcell_rnaseq/2-mbv/plot.R
@@ -22,7 +22,8 @@ mbv_df <-
 	   n_hom_covered, 
 	   perc_het_consistent,
 	   perc_hom_consistent) |>
-    mutate(rnaseq_stim = factor(rnaseq_stim, levels = c("unstday0", "BCR", "TLR7", "DN2")))
+    mutate(rnaseq_stim = recode(rnaseq_stim, "unstday0" = "Day 0"),
+	   rnaseq_stim = factor(rnaseq_stim, levels = c("Day 0", "BCR", "TLR7", "DN2")))
 
 label_df <- 
     mbv_df |>
@@ -32,16 +33,21 @@ label_df <-
     filter(rnaseq_donor_id != vcf_donor_id)
 
 mbv_plot <- 
-    ggplot(mbv_df, 
-	   aes(x = perc_het_consistent, y = perc_hom_consistent)) +
-    geom_point(aes(color = rnaseq_donor_id == vcf_donor_id)) +
+    ggplot(data = mbv_df,
+	   aes(x = perc_het_consistent, 
+	       y = perc_hom_consistent,
+	       color = rnaseq_donor_id == vcf_donor_id)) +
+    geom_point(data = filter(mbv_df, rnaseq_donor_id != vcf_donor_id),
+	       size = 2) +
+    geom_point(data = filter(mbv_df, rnaseq_donor_id == vcf_donor_id),
+	       size = 2) +
     scale_x_continuous(limits = c(0, 1),
 		       breaks = c(0, 1),
 		       labels = c("0", "1")) +
     scale_y_continuous(limits = c(0, 1),
 		       breaks = c(0, 1),
 		       labels = c("0", "1")) +
-    scale_color_manual(values = c("TRUE" = "Lime Green", "FALSE" = "tomato3")) +
+    scale_color_manual(values = c("TRUE" = "Yellow Green", "FALSE" = "Dark Red")) +
     geom_text_repel(data = label_df, 
 		    aes(x = perc_het_consistent, 
 			y = perc_hom_consistent, 
@@ -51,31 +57,30 @@ mbv_plot <-
 		    nudge_y = -.5,
 		    segment.size = .2,
 		    size = 3) +
-    facet_grid(rnaseq_sample_id ~ rnaseq_stim) +
+    facet_grid(rnaseq_stim ~ sub("_", "\n", rnaseq_sample_id)) +
     theme_bw() +
     theme(axis.text = element_text(size = 10),
 	  axis.title = element_text(size = 12),
 	  panel.grid = element_blank(),
-	  legend.margin = margin(0, 0, 0, 0),
-	  legend.position = "top",
-	  strip.text.y = element_text(angle = 0),
+	  panel.spacing = unit(0.2, "lines"),
+	  strip.text.x = element_text(size = 10),
+	  strip.text.y = element_text(angle = 0, size = 11),
 	  strip.background = element_rect(fill = "white", color = "white")) +
-    labs(x = "Proportion consistent Hets", 
-	 y = "Proportion consistent Homs",
-	 color = "Match:") +
-    guides(color = guide_legend(override.aes = list(size = 2)))
+    labs(x = "Proportion consistent Heterozygous sites", 
+	 y = "Proportion consistent Homs") +
+    guides(color = "none")
 
-ggsave("./mbv.png", mbv_plot, width = 4.5, height = 11.5)
+ggsave("./mbv.png", mbv_plot, width = 15, height = 3.15)
 
 
-# Save matching results
-mbv_df |>
-    group_by(vcf_donor_id) |>
-    mutate(n_batches = n_distinct(vcf_batch)) |>
-    filter(n_batches == 1 | vcf_batch != "0410") |>
-    group_by(rnaseq_sample_id, rnaseq_stim) |>
-    slice_max(perc_het_consistent + perc_hom_consistent) |>
-    ungroup() |>
-    select(rnaseq_sample_id, rnaseq_stim, vcf_donor_id, vcf_sample_id, vcf_batch) |>
-    write_tsv("matching_results.tsv")
-
+## Save matching results
+#mbv_df |>
+#    group_by(vcf_donor_id) |>
+#    mutate(n_batches = n_distinct(vcf_batch)) |>
+#    filter(n_batches == 1 | vcf_batch != "0410") |>
+#    group_by(rnaseq_sample_id, rnaseq_stim) |>
+#    slice_max(perc_het_consistent + perc_hom_consistent) |>
+#    ungroup() |>
+#    select(rnaseq_sample_id, rnaseq_stim, vcf_donor_id, vcf_sample_id, vcf_batch) |>
+#    write_tsv("matching_results.tsv")
+#
