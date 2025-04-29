f4689fb93029b921f753ca029b7f56b5d16853ac
VitorAguiar <vitor@ib.usp.br>
2025-04-29 17:17:23 -0400

    update

---
+++ bcell_rnaseq/4-splicing/cluster_introns.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+source /programs/biogrids/biogrids.shrc
+export PYTHON_X=2.7.2
+export R_X=4.1
+
+JUNC=./data/junction_files.txt
+
+# Intron clustering
+python $HOME/leafcutter/clustering/leafcutter_cluster_regtools.py \
+    -j $JUNC \
+    -o ./data/clusters
+
+#rm *.leafcutter.sorted.gz
--- bcell_rnaseq/4-splicing/setup.R
+++ bcell_rnaseq/4-splicing/setup.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 
+# Exclude library 231227 because some files might be corrupt
 meta <- 
     "../0-qc_rnaseq/metadata.tsv" |>
     read_tsv(col_types = "ccccc") |>
-    separate_rows(R1:R2, sep = ",")
+    separate_rows(R1:R2, sep = ",") |>
+    filter(!grepl("/231227", R1))
 
 write_tsv(meta, "./metadata.tsv")
 
@@ -14,6 +16,7 @@ mbv <-
     separate(rnaseq_sample_id, c("donor_id", "replic_id"), sep = "_") |>
     select(donor_id, replic_id, stim = rnaseq_stim, vcf_donor_id)
 
+# QCed fastqs for mapping
 meta_qced <- 
     meta |>
     mutate_at(vars(R1:R2), basename) |>
@@ -29,3 +32,47 @@ meta_qced <-
     select(sample_id, R1, R2)
 
 write_tsv(meta_qced, "./metadata_qced.tsv")
+
+# Leafcutter
+if (!file.exists("data")) dir.create("data")
+if (!file.exists("results")) dir.create("results")
+if (!file.exists("results/TLR7")) dir.create("results/TLR7")
+if (!file.exists("results/BCR")) dir.create("results/BCR")
+if (!file.exists("results/DN2")) dir.create("results/DN2")
+
+meta_qced |>
+    select(sample_id) |>
+    mutate(juncfile = paste0(getwd(), "/bam/", sample_id, ".junc")) |>
+    pull(juncfile) |>
+    write_lines("./data/junction_files.txt")
+
+groups_file_tlr7 <-
+    meta_qced |>
+    select(sample_id) |>
+    separate(sample_id, c("stim", "donor", "replic"), sep = "_", remove = FALSE) |>
+    filter(stim %in% c("unstday0", "TLR7")) |>
+    mutate(stim = factor(stim, levels = c("unstday0", "TLR7"))) |>
+    arrange(stim, donor, replic) |>
+    select(sample_id, stim)
+
+groups_file_bcr <-
+    meta_qced |>
+    select(sample_id) |>
+    separate(sample_id, c("stim", "donor", "replic"), sep = "_", remove = FALSE) |>
+    filter(stim %in% c("unstday0", "BCR")) |>
+    mutate(stim = factor(stim, levels = c("unstday0", "BCR"))) |>
+    arrange(stim, donor, replic) |>
+    select(sample_id, stim)
+
+groups_file_dn2 <-
+    meta_qced |>
+    select(sample_id) |>
+    separate(sample_id, c("stim", "donor", "replic"), sep = "_", remove = FALSE) |>
+    filter(stim %in% c("unstday0", "DN2")) |>
+    mutate(stim = factor(stim, levels = c("unstday0", "DN2"))) |>
+    arrange(stim, donor, replic) |>
+    select(sample_id, stim)
+
+write_tsv(groups_file_tlr7, "./data/groups_TLR7.txt", col_names = FALSE)
+write_tsv(groups_file_bcr, "./data/groups_BCR.txt", col_names = FALSE)
+write_tsv(groups_file_dn2, "./data/groups_DN2.txt", col_names = FALSE)
