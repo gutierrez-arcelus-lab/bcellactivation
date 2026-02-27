a88c6756f9a6223da5d05d0b54ba78e921dfb62d
VitorAguiar <vitor@ib.usp.br>
2026-02-27 15:33:24 -0500

    compile salmon data with separate replicates

--- bcell_rnaseq/4-splicing/compile_salmon.R
+++ bcell_rnaseq/4-splicing/compile_salmon.R
@@ -13,17 +13,14 @@ gene_tx <-
     select(transcript_id, gene_id, gene_name) |> 
     mutate(gene_id = str_remove(gene_id, "\\.\\d+"))
 
+# Individual libraries
 sample_ids <- 
-    "./data/metadata_qced_pooled.tsv" |>
-    read_tsv(col_types = "c--") |>
+    "./data/metadata_qced.tsv" |>
+    read_tsv() |>
     pull(sample_id)
 
-meta_df <- 
-    tibble(sample_id = sample_ids) |>
-    separate(sample_id, c("stim", "timep", "donor_id"), sep = "\\.", remove = FALSE)
-
 salmon_gene_df <- 
-    glue("./salmon_quant_pooled/{sample_ids}/quant.sf") |>
+    glue("./salmon_quant/{sample_ids}/quant.sf") |>
     setNames(sample_ids) |>
     map_dfr(~read_tsv(.) |> 
 	    left_join(gene_tx, join_by(Name == transcript_id)) |>
@@ -31,7 +28,31 @@ salmon_gene_df <-
 	    summarize_at(vars(NumReads, TPM), sum) |> 
 	    ungroup(),
 	    .id = "sample_id") |>
-    left_join(meta_df, join_by(sample_id)) |>
+    select(sample_id, gene_id, gene_name, read_count = NumReads, tpm = TPM)
+
+write_tsv(salmon_gene_df, "./data/salmon_gene.tsv")
+
+
+# Pooled replicates
+pooled_sample_ids <- 
+    "./data/metadata_qced_pooled.tsv" |>
+    read_tsv(col_types = "c--") |>
+    pull(sample_id)
+
+pooled_meta_df <- 
+    tibble(sample_id = pooled_sample_ids) |>
+    separate(sample_id, c("stim", "timep", "donor_id"), sep = "\\.", remove = FALSE)
+
+pooled_salmon_gene_df <- 
+    glue("./salmon_quant_pooled/{pooled_sample_ids}/quant.sf") |>
+    setNames(pooled_sample_ids) |>
+    map_dfr(~read_tsv(.) |> 
+	    left_join(gene_tx, join_by(Name == transcript_id)) |>
+	    group_by(gene_id, gene_name) |>
+	    summarize_at(vars(NumReads, TPM), sum) |> 
+	    ungroup(),
+	    .id = "sample_id") |>
+    left_join(pooled_meta_df, join_by(sample_id)) |>
     select(donor_id, stim, timep, gene_id, gene_name, read_count = NumReads, tpm = TPM)
 
-write_tsv(salmon_gene_df, "./data/salmon_pooledreps_gene.tsv")
+write_tsv(pooled_salmon_gene_df, "./data/salmon_pooledreps_gene.tsv")
